0e99c3c7051c1c5d94833913bf96a51e91bf218e
priyankarani-eGov <priyanka.rani@egovernments.org>
2021-03-06 20:37:10 +0530

    first commit

---
+++ database-rani/src/main/java/com/rani/study/databaserani/DatabaseRaniApplication.java
@@ -0,0 +1,13 @@
+package com.rani.study.databaserani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseRaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseRaniApplication.class, args);
+	}
+
+}
---
+++ database-rani/src/test/java/com/rani/study/databaserani/DatabaseRaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rani.study.databaserani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseRaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
